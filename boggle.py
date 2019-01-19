21657403943595d584aa0a6e29bf06d9cf4ed42d
Aiden Harvey <aidenharvey102@gmail.com>
2019-01-19 10:36:11 +0000

    refactored code for efficiency

--- boggle.py
+++ boggle.py
@@ -70,11 +70,14 @@ def search(grid, dictionary):
     """
     neighbours = all_grid_neighbours(grid)
     paths = []
+    full_words, stems = dictionary
     
     def do_search(path):
         word = path_to_word(grid, path)
-        if word_in_dictionary(word, dictionary):
+        if word in full_words:
             paths.append(path)
+        if word not in stems:
+            return
         for next_pos in neighbours[path[-1]]:
             if next_pos not in path:
                 do_search(path + [next_pos])
@@ -91,8 +94,16 @@ def get_dictionary(dictionary_file):
     """
     load dictionary file
     """
+    full_words, stems = set(), set()
     with open(dictionary_file) as f:
-        return{w.strip().upper() for w in f}
+        for word in f:
+            word = word.strip().upper()
+            full_words.add(word)
+            
+            for i in range(1, len(word)):
+                stems.add(word[:i])
+        
+    return full_words, stems
 
 def display_words(words):
     """
